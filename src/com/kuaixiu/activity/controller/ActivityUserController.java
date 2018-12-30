b7a98339e4f1bbba5ee3f3fe2e40d995cf682aa9
najy <2369697747@qq.com>
2018-12-30 20:00:36 +0800

    转换机型，获取机型图片名称

--- src/com/kuaixiu/activity/controller/ActivityUserController.java
+++ src/com/kuaixiu/activity/controller/ActivityUserController.java
@@ -1,16 +1,25 @@
 package com.kuaixiu.activity.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.common.base.controller.BaseController;
 import com.common.exception.SystemException;
 import com.common.paginate.Page;
+import com.common.util.AES;
+import com.common.util.FileUtil;
 import com.common.wechat.aes.AesCbcUtil;
 import com.common.wechat.common.util.StringUtils;
 import com.kuaixiu.activity.entity.ActivityProject;
 import com.kuaixiu.activity.entity.ActivityUser;
 import com.kuaixiu.activity.service.ActivityProjectService;
 import com.kuaixiu.activity.service.ActivityUserService;
+import com.kuaixiu.recycle.controller.RecycleController;
+import com.kuaixiu.recycle.entity.RecycleCheckItems;
+import com.kuaixiu.recycle.service.RecycleCheckItemsService;
 import com.system.api.entity.ResultData;
+import com.system.constant.SystemConstant;
+import jodd.util.StringUtil;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +28,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -30,11 +40,21 @@ import java.util.*;
  */
 @Controller
 public class ActivityUserController extends BaseController {
-
+    private static final Logger log = Logger.getLogger(RecycleController.class);
     @Autowired
     private ActivityUserService activityUserService;
     @Autowired
     private ActivityProjectService activityProjectService;
+    @Autowired
+    private RecycleCheckItemsService recycleCheckItemsService;
+    /**
+     * 基础访问接口地址
+     */
+    private static final String baseUrl = SystemConstant.RECYCLE_URL;
+    /**
+     * 需要加密的数据名
+     */
+    private static final String cipherdata = SystemConstant.RECYCLE_REQUEST;
 
     /**
      * 列表查询
@@ -103,12 +123,10 @@ public class ActivityUserController extends BaseController {
                     map.put("businessType", 1);
                     objects.add(map);
                 }
-
             }
             if (StringUtils.isBlank(businessType) || "2".equals(businessType)) {
                 //电信业务
             }
-
             page.setData(objects);
         } catch (Exception e) {
             e.printStackTrace();
@@ -245,6 +263,146 @@ public class ActivityUserController extends BaseController {
             result.setSuccess(true);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 通过微信openId和微信小程序检测出的用户机型和品牌获取机型品牌名称 品牌id 机型名称  机型id  产品id
+     *
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/activityCompany/getProductId")
+    @ResponseBody
+    public ResultData getProductId(HttpServletRequest request,
+                                   HttpServletResponse response) throws Exception {
+        ResultData result = new ResultData();
+        JSONObject jsonResult = new JSONObject();
+        String productId = null;   //通过回收结果返回的最终机型id
+        try {
+            //获取请求数据
+            JSONObject params = getPrarms(request);
+            String brand = params.getString("brand");
+            String modelName = params.getString("modelName");
+            String openId = params.getString("openId");
+            String categoryid = params.getString("categoryid");
+            if (StringUtil.isBlank(brand) || StringUtil.isBlank(modelName) || StringUtil.isBlank(openId)) {
+                throw new SystemException("参数不完整");
+            }
+            //将微信小程序检测的机型名称转换成回收平台的机型名称
+            Map<String, String> map = AES.getModelName(brand, modelName);
+            JSONObject requestNews = new JSONObject();
+            if (map.isEmpty()) {
+                throw new SystemException("对应机型未找到!");
+            }
+            //通过转换过的机型 使用回收搜索接口得到对应机型id
+            JSONObject code = new JSONObject();
+            code.put("pageindex", "1");
+            code.put("pagesize", "50");
+            code.put("categoryid", categoryid);
+            code.put("keyword", map.get("modelName").replaceAll(" ", ""));
+            String realCode = AES.Encrypt(code.toString());  //加密
+            requestNews.put(cipherdata, realCode);
+            //发起获取对应productid请求
+            String getResult = AES.post(baseUrl + "getmodellist", requestNews);
+            //对得到结果进行解密
+            code = getResult(AES.Decrypt(getResult));
+            JSONArray product = code.getJSONArray("datainfo");
+            if (product.isEmpty()) {
+                throw new SystemException("对应机型不存在");
+            }
+            int goodPirce = 0;
+            //判断返回的机型是否有多个
+            if (product.size() > 1) {
+                //如果有多个机型 则通过机型名称匹配唯一
+                for (int i = 0; i < product.size(); i++) {
+                    JSONObject object = (JSONObject) product.get(i);
+                    JSONArray sublist = object.getJSONArray("sublist");
+                    if (((JSONObject) sublist.get(0)).getString("modelname").equals(map.get("modelName"))) {
+                        productId = object.getString("productid");
+                        goodPirce = (int) (object.getInteger("productprice") * 0.9);
+                    }
+                }
+            } else {
+                JSONObject o = (JSONObject) product.get(0);
+                JSONArray sublist = o.getJSONArray("sublist");
+                productId = ((JSONObject) sublist.get(0)).getString("productid");
+                goodPirce = (int) (((JSONObject) sublist.get(0)).getInteger("productprice") * 0.9);
+            }
+            if (StringUtils.isBlank(productId)) {
+                throw new SystemException("对应机型id不存在");
+            }
+
+            //获取该机型的良品价
+            //Map<String, Object> maps = getGoodPirce(map.get("brandId"), productId, categoryid);
+            //int goodPirce = (int) maps.get("maxPrice");
+            //获取该用户该机型上次的评估价
+            Integer lastPrice = null;
+            RecycleCheckItems checkItems = new RecycleCheckItems();
+            checkItems.setWechatId(openId);
+            checkItems.setWechatModel(modelName);
+            List<RecycleCheckItems> checkList = recycleCheckItemsService.queryList(checkItems);
+            if (!checkList.isEmpty() && checkList.get(0).getLastPrice() != null) {
+                lastPrice = checkList.get(0).getLastPrice().intValue();
+            }
+            //将图片的http的链接转换为https返回  图片保存位置   项目根目录的 resource/brandLogo下
+            String imageUrl = map.get("modelLogo");
+            String savePath = serverPath(request.getServletContext().getRealPath("")) + System.getProperty("file.separator") + SystemConstant.IMAGE_PATH + System.getProperty("file.separator") + "recycleModel";
+            log.info("图片保存路径:" + savePath);
+            String modelUrl = getProjectUrl(request) + "/images/recycleModel/" + imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+            //先判断文件是否存在  不存在则下载
+            File f = new File(savePath + System.getProperty("file.separator") + (imageUrl.substring(imageUrl.lastIndexOf("/") + 1)));
+            if (!f.exists()) {
+                log.info("图片不存在，开始下载");
+                FileUtil.download(imageUrl, imageUrl.substring(imageUrl.lastIndexOf("/") + 1), savePath);
+            }
+
+            //返回数据
+            jsonResult.put("brandName", map.get("brandName"));
+            jsonResult.put("brandId", map.get("brandId"));
+            jsonResult.put("modelName", map.get("modelName"));
+            jsonResult.put("productId", productId);
+            jsonResult.put("modelId", map.get("modelId"));
+            jsonResult.put("goodPrice", goodPirce);
+            jsonResult.put("lastPrice", lastPrice);
+            jsonResult.put("modelLogo", modelUrl);
+            result.setResult(jsonResult);
+            result.setResultCode("0");
+            result.setSuccess(true);
+
+
+            //修改
+            RecycleCheckItems cr = checkList.get(0);
+            cr.setBrandId(map.get("brandId"));
+            cr.setModelId(map.get("modelId"));
+            cr.setRecycleModel(map.get("modelName"));
+            cr.setWechatModel(modelName);
+            cr.setBrand(brand);
+            cr.setProductId(productId);
+            recycleCheckItemsService.saveUpdate(cr);
+
+
+            result.setResultCode("0");
+            result.setSuccess(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 返回结果解析
+     *
+     * @param originalString
+     * @return
+     */
+    public static JSONObject getResult(String originalString) {
+        JSONObject result = (JSONObject) JSONObject.parse(originalString);
+        if (result.getString("result") != null && !result.getString("result").equals("RESPONSESUCCESS")) {
+            throw new SystemException(result.getString("msg"));
         }
         return result;
     }
