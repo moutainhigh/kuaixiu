d9b8b33b15cd690a0178095e30a0643a5b25f29f
najy <2369697747@qq.com>
2019-04-19 16:59:12 +0800

    增加返回注解responseBody

--- src/com/kuaixiu/recycle/controller/RecycleNewController.java
+++ src/com/kuaixiu/recycle/controller/RecycleNewController.java
@@ -18,6 +18,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -885,6 +886,7 @@ public class RecycleNewController extends BaseController {
      * 获取订单列表
      */
     @RequestMapping(value = "recycleNew/getOrderList")
+    @ResponseBody
     public ResultData getOrderList(HttpServletRequest request, HttpServletResponse response) throws Exception {
         ResultData result = new ResultData();
         JSONObject jsonResult = new JSONObject();
