e49ea0694bcf95b7cd650d4ed732afb87ed40776
najy <2369697747@qq.com>
2019-04-22 14:54:03 +0800

    获取回收订单列表接口改结构加注解

--- src/com/kuaixiu/recycle/controller/RecycleController.java
+++ src/com/kuaixiu/recycle/controller/RecycleController.java
@@ -22,6 +22,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
@@ -1026,6 +1027,7 @@ public class RecycleController extends BaseController {
      * 获取订单列表
      */
     @RequestMapping(value = "recycle/getOrderList")
+    @ResponseBody
     public ResultData getOrderList(HttpServletRequest request, HttpServletResponse response) throws Exception {
         ResultData result = new ResultData();
         JSONObject jsonResult = new JSONObject();
