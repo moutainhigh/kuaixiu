37119025f8d7ad548571135b57aebb2a88e81a72
gqa <657182420@qq.com>
2019-08-16 13:24:25 +0800

    5G活动

--- src/com/kuaixiu/videoCard/controller/VideoCardController.java
+++ src/com/kuaixiu/videoCard/controller/VideoCardController.java
@@ -1,107 +1,156 @@
-package com.kuaixiu.videoCard.controller;
-
-import com.alibaba.fastjson.JSONObject;
-import com.common.base.controller.BaseController;
-import com.google.common.collect.Maps;
-import com.kuaixiu.order.constant.OrderConstant;
-import com.kuaixiu.videoCard.entity.VideoCard;
-import com.kuaixiu.videoCard.service.VideoCardService;
-import com.kuaixiu.videoUserRel.entity.VideoUserRel;
-import com.kuaixiu.videoUserRel.service.VideoUserRelService;
-import com.system.basic.user.entity.SessionUser;
-import com.system.constant.SystemConstant;
-import com.system.util.ExcelUtil;
-import jodd.util.StringUtil;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
-/**
- * VideoCard Controller
- *
- * @CreateDate: 2019-08-15 下午03:37:26
- * @version: V 1.0
- */
-@Controller
-public class VideoCardController extends BaseController {
-
-    @Autowired
-    private VideoCardService videoCardService;
-
-
-    /**
-     * 列表查询
-     *
-     * @param request
-     * @param response
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(value = "/videoCard/list")
-    public ModelAndView list(HttpServletRequest request,
-                              HttpServletResponse response) throws Exception {
-
-        String returnView ="videoCard/videoCardList";
-        return new ModelAndView(returnView);
-    }
-
-
-
-
-    @RequestMapping(value="/videoCard/import")
-    public ModelAndView cardImport(HttpServletRequest request,HttpServletResponse response){
-        return new ModelAndView("videoCard/importIndex");
-    }
-
-
-
-
-    @RequestMapping("/videoCard/personList")
-    @ResponseBody
-    public void personList(HttpServletRequest request,
-                            HttpServletResponse response) throws IOException {
-        SessionUser su = getCurrentUser(request);
-        String loginId = su.getUser().getLoginId();
-        List<VideoCard> list=new ArrayList<>();
-        if(!StringUtils.isBlank(loginId)){
-            VideoUserRel rel=new VideoUserRel();
-            rel.setMobile(loginId);
-            list=videoCardService.getVideoUser(rel);
-        }
-        Map<String, Object> resultMap = Maps.newHashMap();
-        resultMap.put(RESULTMAP_KEY_SUCCESS, RESULTMAP_SUCCESS_TRUE);
-        resultMap.put("data", list);
-        renderJson(response, resultMap);
-    }
-
-
-
-
-
-    public static void projectNews() throws Exception {
-        String file = "E:\\code\\爱奇艺卡密类型.xlsx";
-        Object o = ExcelUtil.testProjectList(file);
-        List<VideoCard> list = (List<VideoCard>) o;   //获取excel信息
-        System.out.println("卡密信息："+ JSONObject.toJSONString(list));
-
-
-
-    }
-
-
-    public static void main(String[] args) throws Exception {
-        projectNews();
-    }
-
-
-}
+//package com.kuaixiu.videoCard.controller;
+//
+//import com.alibaba.fastjson.JSONObject;
+//import com.common.base.controller.BaseController;
+//import com.common.importExcel.ImportReport;
+//import com.google.common.collect.Maps;
+//import com.kuaixiu.order.constant.OrderConstant;
+//import com.kuaixiu.videoCard.entity.VideoCard;
+//import com.kuaixiu.videoCard.service.VideoCardService;
+//import com.kuaixiu.videoUserRel.entity.VideoUserRel;
+//import com.kuaixiu.videoUserRel.service.VideoUserRelService;
+//import com.system.basic.user.entity.SessionUser;
+//import com.system.constant.SystemConstant;
+//import com.system.util.ExcelUtil;
+//import jodd.util.StringUtil;
+//import org.apache.commons.io.FilenameUtils;
+//import org.apache.commons.lang3.StringUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.multipart.MultipartFile;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.math.BigDecimal;
+//import java.text.SimpleDateFormat;
+//import java.util.*;
+//
+///**
+// * VideoCard Controller
+// *
+// * @CreateDate: 2019-08-15 下午03:37:26
+// * @version: V 1.0
+// */
+//@Controller
+//public class VideoCardController extends BaseController {
+//
+//    @Autowired
+//    private VideoCardService videoCardService;
+//
+//
+//    /**
+//     * 列表查询
+//     *
+//     * @param request
+//     * @param response
+//     * @return
+//     * @throws Exception
+//     */
+//    @RequestMapping(value = "/videoCard/list")
+//    public ModelAndView list(HttpServletRequest request,
+//                              HttpServletResponse response) throws Exception {
+//
+//        String returnView ="videoCard/videoCardList";
+//        return new ModelAndView(returnView);
+//    }
+//
+//
+//
+//
+//    @RequestMapping(value="/videoCard/import")
+//    public ModelAndView cardImport(HttpServletRequest request,HttpServletResponse response){
+//        return new ModelAndView("videoCard/importIndex");
+//    }
+//
+//
+//
+//    /**
+//     * 卡密模板导入
+//     * @param myfile
+//     * @param request
+//     * @param response
+//     * @throws IOException
+//     */
+//    @RequestMapping(value = "/videoCard/startImport")
+//    public void doImport(
+//            @RequestParam("fileInput") MultipartFile myfile,
+//            HttpServletRequest request, HttpServletResponse response)
+//            throws IOException {
+//        // 返回结果，默认失败
+//        Map<String, Object> resultMap = new HashMap<String, Object>();
+//        resultMap.put(RESULTMAP_KEY_SUCCESS, RESULTMAP_SUCCESS_FALSE);
+//        ImportReport report = new ImportReport();
+//        StringBuffer errorMsg = new StringBuffer();
+//        try{
+//            if(myfile != null && StringUtils.isNotBlank(myfile.getOriginalFilename())){
+//                String fileName=myfile.getOriginalFilename();
+//                //扩展名
+//                String extension= FilenameUtils.getExtension(fileName);
+//                if (!extension.equalsIgnoreCase("xls")){
+//                    errorMsg.append("导入文件格式错误！只能导入excel  xls文件！");
+//                }
+//                else{
+////                    stationService.importExcel(myfile,report,getCurrentUser(request));
+//                    resultMap.put(RESULTMAP_KEY_SUCCESS, RESULTMAP_SUCCESS_TRUE);
+//                    resultMap.put(RESULTMAP_KEY_MSG, "导入成功");
+//                }
+//            }
+//            else{
+//                errorMsg.append("导入文件为空");
+//            }
+//        }
+//        catch(Exception e) {
+//            e.printStackTrace();
+//            errorMsg.append("导入失败");
+//            resultMap.put(RESULTMAP_KEY_MSG, "导入失败");
+//        }
+//        request.setAttribute("report", report);
+//        resultMap.put(RESULTMAP_KEY_DATA, report);
+//        renderJson(response, resultMap);
+//    }
+//
+//
+//    @RequestMapping("/videoCard/personList")
+//    @ResponseBody
+//    public void personList(HttpServletRequest request,
+//                            HttpServletResponse response) throws IOException {
+//        SessionUser su = getCurrentUser(request);
+//        String loginId = su.getUser().getLoginId();
+//        List<VideoCard> list=new ArrayList<>();
+//        if(!StringUtils.isBlank(loginId)){
+//            VideoUserRel rel=new VideoUserRel();
+//            rel.setMobile(loginId);
+//            list=videoCardService.getVideoUser(rel);
+//        }
+//        Map<String, Object> resultMap = Maps.newHashMap();
+//        resultMap.put(RESULTMAP_KEY_SUCCESS, RESULTMAP_SUCCESS_TRUE);
+//        resultMap.put("data", list);
+//        renderJson(response, resultMap);
+//    }
+//
+//
+//
+//
+//
+//    public static void projectNews() throws Exception {
+//        String file = "E:\\code\\爱奇艺卡密类型.xlsx";
+//        Object o = ExcelUtil.testProjectList(file);
+//        List<VideoCard> list = (List<VideoCard>) o;   //获取excel信息
+//        System.out.println("卡密信息："+ JSONObject.toJSONString(list));
+//
+//
+//
+//    }
+//
+//
+//    public static void main(String[] args) throws Exception {
+//        projectNews();
+//    }
+//
+//
+//}
--- src/com/kuaixiu/videoCard/service/VideoCardService.java
+++ src/com/kuaixiu/videoCard/service/VideoCardService.java
@@ -1,37 +1,270 @@
-package com.kuaixiu.videoCard.service;
-
-
-import com.common.base.service.BaseService;
-import com.kuaixiu.videoCard.dao.VideoCardMapper;
-import com.kuaixiu.videoCard.entity.VideoCard;
-
-import com.kuaixiu.videoUserRel.entity.VideoUserRel;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-/**
- * VideoCard Service
- * @CreateDate: 2019-08-15 下午03:37:26
- * @version: V 1.0
- */
-@Service("videoCardService")
-public class VideoCardService extends BaseService<VideoCard> {
-    private static final Logger log= Logger.getLogger(VideoCardService.class);
-
-    @Autowired
-    private VideoCardMapper<VideoCard> mapper;
-
-
-    public VideoCardMapper<VideoCard> getDao() {
-        return mapper;
-    }
-
-    //**********自定义方法***********
-    public List<VideoCard> getVideoUser(VideoUserRel rel) {
-        return mapper.getVideoUser(rel);
-    }
-
-}
+//package com.kuaixiu.videoCard.service;
+//
+//
+//import com.common.base.service.BaseService;
+//import com.common.exception.SystemException;
+//import com.common.importExcel.ExcelUtil;
+//import com.common.importExcel.ImportError;
+//import com.common.importExcel.ImportReport;
+//import com.kuaixiu.station.entity.Station;
+//import com.kuaixiu.videoCard.dao.VideoCardMapper;
+//import com.kuaixiu.videoCard.entity.VideoCard;
+//
+//import com.kuaixiu.videoUserRel.entity.VideoUserRel;
+//import com.system.basic.user.entity.SessionUser;
+//import net.sf.jxls.exception.ParsePropertyException;
+//import org.apache.commons.io.FilenameUtils;
+//import org.apache.commons.lang3.StringUtils;
+//import org.apache.log4j.Logger;
+//import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+//import org.apache.poi.ss.usermodel.Row;
+//import org.apache.poi.ss.usermodel.Sheet;
+//import org.apache.poi.ss.usermodel.Workbook;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.FileInputStream;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.text.SimpleDateFormat;
+//import java.util.*;
+//
+///**
+// * VideoCard Service
+// * @CreateDate: 2019-08-15 下午03:37:26
+// * @version: V 1.0
+// */
+//@Service("videoCardService")
+//public class VideoCardService extends BaseService<VideoCard> {
+//    private static final Logger log= Logger.getLogger(VideoCardService.class);
+//
+//    @Autowired
+//    private VideoCardMapper<VideoCard> mapper;
+//
+//
+//    public VideoCardMapper<VideoCard> getDao() {
+//        return mapper;
+//    }
+//
+//    //**********自定义方法***********
+//    public List<VideoCard> getVideoUser(VideoUserRel rel) {
+//        return mapper.getVideoUser(rel);
+//    }
+//
+//
+//    @Transactional
+//    public void importExcel(MultipartFile file, ImportReport report, SessionUser su){
+//        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+//        Workbook workbook = null;
+//        InputStream inputStream = null;
+//        try {
+//            inputStream = file.getInputStream();
+//            // 根据后缀实例化，xls实例化HSSFWorkbook,xlsx实例化XSSFWorkbook
+//            if (extension.equalsIgnoreCase("xls")){
+//                workbook = new HSSFWorkbook(inputStream);
+//            }
+//            else {
+//                workbook = new XSSFWorkbook(inputStream);
+//            }
+//            //检查模板是否正确
+//            if(checkExcelModel(workbook,report)){
+//                //检查表格数据
+//                List<VideoCard> list=checkData(workbook, report, su);
+//                if(report.isPass() && list.size() > 0){
+//                    //保存数据
+//                    saveData(list, su);
+//                }
+//            }
+//            else{
+//                report.setContinueNext(false);
+//                report.setPass(false);
+//            }
+//        }
+//        catch (SystemException e) {
+//            e.printStackTrace();
+//            report.setPass(false);
+//            report.setContinueNext(false);
+//            report.setError("导入错误："+ e.getMessage());
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//            report.setPass(false);
+//            report.setContinueNext(false);
+//            report.setError("系统异常请联系管理员");
+//        }
+//        finally{
+//            if(inputStream!=null){
+//                try {
+//                    inputStream.close();
+//                }
+//                catch (IOException e) {
+//
+//                }
+//            }
+//        }
+//
+//    }
+//
+//
+//    @Transactional
+//    public void saveData(List<VideoCard> list, SessionUser su){
+//        for(VideoCard s:list){
+//            mapper.add(s);
+//        }
+//    }
+//
+//    /**
+//     * 导入模板字段
+//     */
+//    private static Map<Integer,String> titleMap=new HashMap<Integer, String>();
+//    static {
+//        titleMap.put(0, "站点id");
+//        titleMap.put(1, "站点名称");
+//        titleMap.put(2, "联系人");
+//        titleMap.put(3, "验机电话");
+//        titleMap.put(4, "地址");
+//    }
+//
+//
+//    /**
+//     * 检查模板是否正确
+//     * @param workbook
+//     * @return
+//     */
+//    private boolean checkExcelModel(Workbook workbook, ImportReport report){
+//        Sheet sheet=workbook.getSheetAt(0);
+//        Row row0=sheet.getRow(0);
+//        if(row0 == null){
+//            return false;
+//        }
+//        //模板数据
+//        Set<Integer> set = titleMap.keySet();
+//        for (Integer key : set) {
+//            String t1=row0.getCell(key).toString().trim();
+//            String t2=titleMap.get(key);
+//            if(t1==null||!t1.equals(t2)){
+//                report.setPass(false);
+//                report.setContinueNext(false);
+//                report.setError("导入模板不正确");
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//
+//    /**
+//     * 检查表格数据
+//     * @param workbook
+//     * @param report
+//     * @param su
+//     * @return
+//     */
+//    private List<VideoCard> checkData(Workbook workbook,ImportReport report,SessionUser su){
+//        SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd");
+//        Sheet sheet=workbook.getSheetAt(0);
+//        int rowNum=sheet.getLastRowNum();
+//        //添加的站点集合
+//        List<VideoCard> stationList=new ArrayList<VideoCard>();
+//
+//        for(int i = 1; i <= rowNum; i++){
+//            Row row = sheet.getRow(i);
+//            if(row==null){
+//                continue;
+//            }
+//            VideoCard s=new VideoCard();
+//
+//            int col = 0;
+//            //站点id
+//            String value = ExcelUtil.getCellValue(row, col).trim();
+//            if (StringUtils.isBlank(value) || value.length() >36) {
+//                ImportError error = new ImportError();
+//                error.setPosition("第" + (row.getRowNum() + 1) + "行," + (col + 1) + "列");
+//                error.setMessage("站点id不能为空，长度不能超过36个字符！");
+//                report.getErrorList().add(error);
+//                report.setPass(false);
+//            } else {
+////                s.setId(value);
+//            }
+//            //站点名称
+//            col++;
+//            value = ExcelUtil.getCellValue(row, col).trim();
+//            if (StringUtils.isBlank(value) || value.length() > 36) {
+//                ImportError error = new ImportError();
+//                error.setPosition("第" + (row.getRowNum() + 1) + "行," + (col + 1) + "列");
+//                error.setMessage("站点名称不能为空，长度不能超过36个字符！");
+//                report.getErrorList().add(error);
+//                report.setPass(false);
+//            } else {
+//                s.setStationName(value);
+//            }
+//            //联系人
+//            col++;
+//            value = ExcelUtil.getCellValue(row, col).trim();
+//            if (StringUtils.isBlank(value) || value.length() > 36) {
+//                ImportError error = new ImportError();
+//                error.setPosition("第" + (row.getRowNum() + 1) + "行," + (col + 1) + "列");
+//                error.setMessage("联系人不能为空，长度不能超过36个字符！");
+//                report.getErrorList().add(error);
+//                report.setPass(false);
+//            } else {
+//                s.setName(value);
+//            }
+//            //验机电话
+//            col++;
+//            value = ExcelUtil.getCellValue(row, col).trim();
+//            if (StringUtils.isBlank(value)||value.length() > 36) {
+//                ImportError error = new ImportError();
+//                error.setPosition("第" + (row.getRowNum() + 1) + "行," + (col + 1) + "列");
+//                error.setMessage("验机电话不能为空，长度不能超过36个字符！");
+//                report.getErrorList().add(error);
+//                report.setPass(false);
+//            } else {
+//                s.setTel(value);
+//            }
+//            //地址
+//            col++;
+//            value = ExcelUtil.getCellValue(row, col).trim();
+//            if (StringUtils.isBlank(value)||value.length() > 64) {
+//                ImportError error = new ImportError();
+//                error.setPosition("第" + (row.getRowNum() + 1) + "行," + (col + 1) + "列");
+//                error.setMessage("地址信息不能为空，长度不能超过64个字符！");
+//                report.getErrorList().add(error);
+//                report.setPass(false);
+//            } else {
+//                s.setAddress(value);
+//            }
+//            stationList.add(s);
+//        }
+//        return stationList;
+//    }
+//
+//
+//
+//    /**
+//     * 下载导入模板
+//     *
+//     * @param params
+//     */
+//    @SuppressWarnings("rawtypes")
+//    public void expImportTemplate(Map<String, Object> params) {
+//        String templateFileName = params.get("tempFileName") + "";
+//        String outFileName = params.get("outFileName") + "";
+//        try {
+//            Workbook workbook = new HSSFWorkbook(new FileInputStream(templateFileName));
+//            FileOutputStream fileOut = new FileOutputStream(outFileName);
+//            workbook.write(fileOut);
+//            fileOut.close();
+//        } catch (ParsePropertyException e) {
+//            e.printStackTrace();
+//            log.error("文件导出--ParsePropertyException", e);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            log.error("文件导出--IOException", e);
+//        }
+//    }
+//}
